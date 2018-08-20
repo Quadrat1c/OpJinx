6a9b55336cbe28602933f3a5e11a5461d02d3cb6
quadrat1c <w4rd3nbot@gmail.com>
2018-08-20 10:22:29 -0700

    Create CustomObjects.cs

---
+++ JinxCrawler/WebCrawler/CustomObjects.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace OpJinx.Web
+{
+    // Generic Object
+    public class GEN
+    {
+        public string header { get; set; }
+        public string link { get; set; }
+    }
+}
